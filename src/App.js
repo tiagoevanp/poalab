709f44f15cf523b536040b157f4de48ae5af812f
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-12-12 19:17:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Image from './resources/logo_poalab.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+	<div className="App">
+		<p>Hello World</p>
+		<img src={Image} alt='logo poalab'/>
+	</div>
+);
 
 export default App;
